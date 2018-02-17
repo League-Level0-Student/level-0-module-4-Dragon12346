186e3fb2b03b61a5961548068e7e2e9cb831b86c
Dragon12346 <league@WorthyMini-6.attlocal.net>
2018-02-17 11:23:05 -0800

    FruitQ(WIP), Dontforgetthelyrics

--- src/DontForgetTheLyrics.java
+++ src/DontForgetTheLyrics.java
@@ -20,17 +20,32 @@ public class DontForgetTheLyrics {
 	
 	public static void main(String[] args) {
 		// 4. Make a pop-up to explain the game.
-
+		JOptionPane.showMessageDialog(null, "For this game, we'll play the start of a song, and you have to guess the rest of the lyrics");
 		// 5. Use the playSound method to play your song.
-
+		playSound("cen.wav");
 		// 6. Make a pop-up for the player to type their answer.
-
+		String guess = JOptionPane.showInputDialog("What is the name of the song?");
 		// 7. If they answered correctly, tell them that they were right.
-
+		if (guess.equals("centuries")) {
+			JOptionPane.showMessageDialog(null, "You won!?!?!?!?!");
+		}
 		// 8. Otherwise, tell them they are wrong, and give them the answer.
-
+		else {JOptionPane.showMessageDialog(null, "You lost!");
+		
+		}
 		// 9. Record another sound and repeat steps 5-8.
 
+		playSound("hall.wav");
+		// 6. Make a pop-up for the player to type their answer.
+		String guesstow = JOptionPane.showInputDialog("What is the name of the song?");
+		// 7. If they answered correctly, tell them that they were right.
+		if (guesstow.equals("hallelujah")) {
+			JOptionPane.showMessageDialog(null, "You won!?!?!?!?!");
+		}
+		// 8. Otherwise, tell them they are wrong, and give them the answer.
+		else {JOptionPane.showMessageDialog(null, "You lost!");
+		
+		}
 		// 10. [optional] Add a points variable that will calculate their final score.
 	}
 
--- src/FruitQuiz.java
+++ src/FruitQuiz.java
@@ -23,12 +23,12 @@ public class FruitQuiz extends KeyAdapter {
 	public void keyPressed(KeyEvent arg0) {
 		int keyCode = arg0.getKeyCode();
 		// 1. Print out the key code variable
-		
+		System.out.println(keyCode);
 
 		// 2. Make 3 variables that hold the key codes for apple, banana and carrot
-
-		
-
+		String a = "apple";
+		String b = "banana";
+		String c = "carrot";
 		
 		if (question1.isShowing()) {
 			// 3. If they touched the right fruit, do steps 4 and 7
